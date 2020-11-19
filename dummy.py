42b40f059074d7d077946cda177ffdd35f82e9d8
Narmadha Umapathy <rrguhan6@gmail.com>
2020-11-19 18:16:21 +0530

    stack prob

--- dummy.py
+++ dummy.py
@@ -54,3 +54,24 @@ asteroidCollision([5, 10, 20, -50])
 # 5 10 20 - 50
 
 # 15 0 0 0
+
+
+# def asteroidCollision(asteroids):
+#     ans = []
+#     for new in asteroids:
+#         while ans and new < 0 < ans[-1]:
+#             if ans[-1] < -new:
+#                 ans.pop()
+#                 continue
+#             elif ans[-1] == -new:
+#                 ans.pop()
+#             break
+#         else:
+#             ans.append(new)
+#     return ans
+
+
+# # print(asteroidCollision([-2, -2, 1, -2]))
+# # print(asteroidCollision([-2, 1, -2, -2]))
+# print(asteroidCollision([-2, 1, 1, -2]))
+# # print(asteroidCollision([5, 10, 20, -50]))
