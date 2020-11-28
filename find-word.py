716006158226fca3ed5a550d3b1362f63a8d1898
rrguhan6 <rrguhan6@gmail.com>
2020-11-28 18:12:36 +0530

    set problem

---
+++ find-word.py
@@ -0,0 +1,18 @@
+def findWords(words):
+    keyboard = [
+        set('qwertyuiop'),
+        set('asdfghjkl'),
+        set('zxcvbnm')
+    ]
+
+    ans = []
+
+    for w in words:
+        for row in keyboard:
+            if set(w.lower()).issubset(row):
+                ans.append(w)
+
+    return ans
+
+
+print(findWords(["Hello", "Alaska", "Dad", "Peace"]))
